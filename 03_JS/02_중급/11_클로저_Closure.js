9054b5346ab3be69d0a5dbea9d2520f78077e20c
Do-code-ing <zxcvbn658@naver.com>
2021-07-27 17:45:36 +0900

    2021-07-27

---
+++ 03_JS/02_중급/11_클로저_Closure.js
@@ -0,0 +1,20 @@
+// 클로저 (Closure)
+
+// 함수와 렉시컬 환경의 조합
+// 함수가 생성될 당시의 외부 변수를 기억,
+// 생성 이후에도 계속 접근 가능
+
+function makeCounter() {
+    let num = 0; // 은닉화
+    return function () {
+        return num++;
+    };
+}
+
+let counter = makeCounter();
+console.log(counter());
+// 0
+console.log(counter());
+// 1
+console.log(counter());
+// 2
